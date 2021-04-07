c00a48b17b774114980cf43d8a18d385439bba58
Jesus Olaya <jaolaya923@misena.edu.co>
2021-04-07 16:53:13 -0500

    enlaces

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mi Segunda Pagina</title>
+    <title>Mi Segunda Pagina Web</title>
 </head>
 <body style="background: grey">
 <h1>Principal</h1>
